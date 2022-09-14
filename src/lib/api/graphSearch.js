447f5b19566d89f0b1e6681b213e4039816386db
tlmn <mail@tlmn.io>
2022-09-14 16:45:07 +0200

    rewrite with async await #3

--- src/lib/api/graphSearch.js
+++ src/lib/api/graphSearch.js
@@ -1,40 +1,30 @@
-import { Fetcher, Namespace, Store, SPARQLToQuery } from "rdflib";
+import { Fetcher, Store, SPARQLToQuery } from "rdflib";
 import { console } from "next/dist/compiled/@edge-runtime/primitives/console";
 
 const store = new Store();
 const fetcher = new Fetcher(store);
 
-// load ttl/rdf file
-export const load = () => {
+export const load = async () => {
   const url =
     "https://raw.githubusercontent.com/apache/jena/main/jena-permissions/src/example/resources/org/apache/jena/security/example/example.ttl";
-  fetcher.nowOrWhenFetched(url, (ok, body, response) => {
-    if (!ok) {
-      console.log("Oops, something happened and couldn't fetch data " + body);
-    } else if (response.onErrorWasCalled || response.status !== 200) {
-      console.log(
-        "    Non-HTTP error reloading data! onErrorWasCalled=" +
-          response.onErrorWasCalled +
-          " status: " +
-          response.status
-      );
-    } else {
-      console.log("---data loaded---");
+  try {
+    await fetcher.load(url);
 
-      const sparqlQuery = `SELECT *
-            WHERE {
-                ?subject ?predicate ?object .
-            }
-            LIMIT 10`;
+    const queryString = `SELECT *
+                WHERE {
+                    ?subject ?predicate ?object .
+                }
+                LIMIT 10`;
 
-      const query = SPARQLToQuery(sparqlQuery, false, store);
+    const query = SPARQLToQuery(queryString, false, store);
 
-      store.query(query, (result) => {
-        console.log("query ran");
-        console.log("result: ",result);
-      });
-    }
-  });
+    store.query(query, (result) => {
+      console.log("query ran");
+      console.log("query result: ", result);
+    });
+  } catch (error) {
+    console.log("Oops, something happened: ", error);
+  }
 };
 
 // search nodes by name
