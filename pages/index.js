e2808ff951dd92d29a7970d42e45a12480043750
tlmn <mail@tlmn.io>
2022-09-20 15:08:15 +0200

    remove button

--- pages/index.js
+++ pages/index.js
@@ -37,37 +37,6 @@ const IndexPage = () => {
 
   return (
     <div className="w-screen h-screen relative">
-      <button
-        onClick={() => {
-          const uuid = uuidv4();
-          setAppState((prev) => ({
-            ...prev,
-            graph: {
-              ...prev.graph,
-              data: {
-                nodes: [
-                  ...prev.graph.data.nodes,
-                  {
-                    id: uuid,
-                    name: "Image",
-                    type: "inputType",
-                  },
-                ],
-                links: [
-                  ...prev.graph.data.links,
-                  {
-                    source: "t-10",
-                    target: uuid,
-                  },
-                ],
-              },
-            },
-          }));
-        }}
-        className="py-2 px-4 bg-slate-200 hover:bg-slate-600 absolute top-0 left-0"
-      >
-        add
-      </button>
       <div className="flex h-full">
         <div className="h-full w-full">
           <GraphRenderer />
