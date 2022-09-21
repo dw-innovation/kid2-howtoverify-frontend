96e92c2ab13ef6dd615436c0402cb29ae01af3e2
tlmn <mail@tlmn.io>
2022-09-21 15:06:19 +0200

    add styles

---
+++ src/lib/const.js
@@ -0,0 +1,6 @@
+export const STYLES = [
+  { nodeType: "inputType", properties: { color: "green", size: 20 } },
+  { nodeType: "question", properties: { color: "yellow", size: 15 } },
+  { nodeType: "workflow", properties: { color: "red", size: 12 } },
+  { nodeType: "tool", properties: { color: "blue", size: 8 } },
+];
