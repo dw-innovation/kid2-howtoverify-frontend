556a352c86fd258cc7b6c1f5a6009ece474436f8
tlmn <mail@tlmn.io>
2022-10-04 17:07:05 +0200

    replace possible domain #27

--- src/lib/lib.js
+++ src/lib/lib.js
@@ -1,5 +1,7 @@
 import { STYLES } from "@/lib/const";
 
 export const getNodeStyle = (nodeType, property) => {
-  return STYLES.filter((style) => style.nodeType === nodeType)[0].properties[property];
+  return STYLES.filter(
+    (style) => style.nodeType === nodeType.replace("http://dw.com/", "")
+  )[0]?.properties[property];
 };
