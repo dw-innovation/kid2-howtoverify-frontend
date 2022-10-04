ecbbfed3236dab78e68e3cb49ccdfc298bcfdd55
tlmn <mail@tlmn.io>
2022-10-04 17:06:32 +0200

    use correct nodeType #27

--- src/lib/const.js
+++ src/lib/const.js
@@ -1,6 +1,7 @@
 export const STYLES = [
-  { nodeType: "inputType", properties: { color: "green", radius: 20 } },
-  { nodeType: "question", properties: { color: "yellow", radius: 15 } },
-  { nodeType: "workflow", properties: { color: "red", radius: 12 } },
-  { nodeType: "tool", properties: { color: "blue", radius: 8 } },
+  { nodeType: "MediaObject", properties: { color: "green", radius: 20 } },
+  { nodeType: "Question", properties: { color: "yellow", radius: 15 } },
+  { nodeType: "SubQuestion", properties: { color: "yellow", radius: 15 } },
+  { nodeType: "Task", properties: { color: "red", radius: 12 } },
+  { nodeType: "SoftwareApplication", properties: { color: "blue", radius: 8 } },
 ];
