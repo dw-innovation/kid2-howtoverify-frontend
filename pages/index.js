9c469de87e44c455ebf63f19f785f11ad53d0aef
tlmn <mail@tlmn.io>
2022-10-11 18:12:37 +0200

    use button component

--- pages/index.js
+++ pages/index.js
@@ -2,9 +2,10 @@ import useAppContext from "src/lib/hooks/useAppContext";
 import GraphRenderer from "src/components/graphRenderer";
 import { loadGraph } from "@/lib/api/lib";
 import { searchByNodes } from "@/lib/api/api/graphSearch";
-import { useEffect, useState, useRef } from "react";
+import { useEffect, useState, useRef, Fragment } from "react";
 import clsx from "clsx";
 import axios from "axios";
+import Button from "@/components/button";
 
 const IndexPage = () => {
   const {
@@ -39,16 +40,7 @@ const IndexPage = () => {
 
   useEffect(() => {
     fetchGraphData();
-
-    /* loadGraph().then(() =>
-      setAppState((prev) => ({
-        ...prev,
-        graph: {
-          ...prev.graph,
-          data: searchByNodes(pathNodes),
-        },
-      }))
-    ); */
+    console.log(pathNodes);
   }, [pathNodes]);
 
   /*  useEffect(() => {
@@ -61,17 +53,45 @@ const IndexPage = () => {
     }));
   }, [pathNodes]); */
 
+  const ROOTNODES = [
+    { label: "image", id: "http://dw.com/image" },
+    { label: "video", id: "http://dw.com/video" },
+    { label: "audio", id: "http://dw.com/audio" },
+    { label: "text", id: "http://dw.com/text" },
+  ];
+
   return (
     <>
-      <button
-        className="bg-slate-200 p-2 m-2 hover:bg-slate-300 absolute top-0 right-0 z-10"
+      <Button
+        className="absolute top-0 right-0 z-10"
         onClick={() => setShowData(!showData)}
       >
-        toggle data view
-      </button>
+        toggle show data
+      </Button>
       <div className="w-screen h-screen relative">
         <div className="flex h-full">
           <div className="h-full w-full">
+            <div className="flex flex-row">
+              {ROOTNODES.map((node, index) => (
+                <Fragment key={index}>
+                  <Button
+                    onClick={() =>
+                      setAppState((prev) => ({
+                        ...prev,
+                        graph: {
+                          ...prev.graph,
+                          pathNodes: JSON.stringify({
+                            click_history: [node.id],
+                          }),
+                        },
+                      }))
+                    }
+                  >
+                    {node.label}
+                  </Button>
+                </Fragment>
+              ))}
+            </div>
             <GraphRenderer />
           </div>
 
@@ -84,9 +104,12 @@ const IndexPage = () => {
             <b>Array of path nodes</b>
             <br />
             <div className="flex flex-col">
-              <textarea cols={60} rows={10} ref={textareaRef}>
-                {pathNodes}
-              </textarea>
+              <textarea
+                cols={60}
+                rows={10}
+                ref={textareaRef}
+                value={pathNodes}
+              />
               <button
                 onClick={() =>
                   setAppState((prev) => ({
