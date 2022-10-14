6d813b36c34491aa3a9b06392bc8c55309478d2d
tlmn <mail@tlmn.io>
2022-10-14 14:20:13 +0200

    fix feedback text

--- src/components/feedbackButton/feedbackButton.js
+++ src/components/feedbackButton/feedbackButton.js
@@ -3,7 +3,7 @@ import React from "react";
 const FeedbackButton = () => (
   <a
     href={`mailto:innovation@dw.com?subject=Feedback&body=${encodeURIComponent(
-      `Hey, I have feedback on the ${window.location.href} Best`
+      `Hey,\n\nI have feedback on the ${window.location.href}\n\nBest`
     )}`}
   >
     send feedback
