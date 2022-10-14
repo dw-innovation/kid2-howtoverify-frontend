528da964e9d1149a289884161b77a57ce73e57e3
tlmn <mail@tlmn.io>
2022-10-14 14:22:10 +0200

    fix build

--- src/components/feedbackButton/feedbackButton.js
+++ src/components/feedbackButton/feedbackButton.js
@@ -3,7 +3,7 @@ import React from "react";
 const FeedbackButton = () => (
   <a
     href={`mailto:innovation@dw.com?subject=Feedback&body=${encodeURIComponent(
-      `Hey,\n\nI have feedback on the ${window.location.href}\n\nBest`
+      `Hey,\n\nI have feedback on the ${typeof window !== "undefined" && window.location.href}\n\nBest`
     )}`}
   >
     send feedback
