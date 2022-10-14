cfaad7b6b4a60db6f1d2bd0ca9b37e9b70d7d058
tlmn <mail@tlmn.io>
2022-10-14 14:01:34 +0200

    add SEO #6

---
+++ src/components/SEO/SEO.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Head from "next/head";
+
+const SEO = () => {
+  return (
+    <Head>
+      <title>KID2</title>
+      <meta charset="UTF-8" />
+      <meta name="description" content="An explorable knowledge base for verification processes." />
+      <meta name="keywords" content="OSINT, verification" />
+      <meta name="author" content="Deutsche Welle Research and Cooperation Projects" />
+    </Head>
+  );
+};
+
+export default SEO;
