cfcf9ed6fa5d825e7f19d46769d2ff0555e524b0
tlmn <mail@tlmn.io>
2022-10-14 14:06:31 +0200

    add favicons #6

--- src/components/SEO/SEO.js
+++ src/components/SEO/SEO.js
@@ -6,9 +6,53 @@ const SEO = () => {
     <Head>
       <title>KID2</title>
       <meta charset="UTF-8" />
-      <meta name="description" content="An explorable knowledge base for verification processes." />
+      <meta
+        name="description"
+        content="An explorable knowledge base for verification processes."
+      />
       <meta name="keywords" content="OSINT, verification" />
-      <meta name="author" content="Deutsche Welle Research and Cooperation Projects" />
+      <meta
+        name="author"
+        content="Deutsche Welle Research and Cooperation Projects"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        href="/images/icons/favicon-16x16.png"
+        sizes="16x16"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        href="/images/icons/favicon-32x32.png"
+        sizes="32x32"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        href="/images/icons/favicon-96x96.png"
+        sizes="96x96"
+      />
+      <link
+        rel="apple-touch-icon"
+        href="/images/icons/favicon-120x120.png"
+        sizes="120x120"
+      />
+      <link
+        rel="apple-touch-icon"
+        href="/images/icons/favicon-152x152.png"
+        sizes="152x152"
+      />
+      <link
+        rel="apple-touch-icon"
+        href="/images/icons/favicon-167x167.png"
+        sizes="167x167"
+      />
+      <link
+        rel="apple-touch-icon"
+        href="/images/icons/favicon-180x180.png"
+        sizes="180x180"
+      />
     </Head>
   );
 };
