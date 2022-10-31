6e435e993bbbac56d1c6c10add8ab81ae621ec7f
tlmn <mail@tlmn.io>
2022-10-31 11:14:35 +0100

    fix feedback button issue

--- src/components/feedbackButton/feedbackButton.js
+++ src/components/feedbackButton/feedbackButton.js
@@ -1,19 +1,28 @@
 import useTranslation from "next-translate/useTranslation";
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const FeedbackButton = () => {
   const { t } = useTranslation("common");
+
+  const [url, setUrl] = useState("");
+
+  useEffect(() => {
+    typeof window !== "undefined" ? setUrl(window.location.href) : "";
+  }, []);
+
   return (
-    <a
-      href={`mailto:${t("feedbackMail")}?subject=${t(
-        "feedbackSubject"
-      )}&body=${encodeURIComponent(
-        t("feedbackBody", {
-          url: typeof window !== "undefined" ? window.location.href : "",
-        })
-      )}`}
-      dangerouslySetInnerHTML={{ __html: t("sendFeedback") }}
-    />
+    <>
+      <a
+        href={`mailto:${t("feedbackMail")}?subject=${t(
+          "feedbackSubject"
+        )}&body=${encodeURIComponent(
+          t("feedbackBody", {
+            url: url,
+          })
+        )}`}
+        dangerouslySetInnerHTML={{ __html: t("sendFeedback") }}
+      />
+    </>
   );
 };
 export default FeedbackButton;
