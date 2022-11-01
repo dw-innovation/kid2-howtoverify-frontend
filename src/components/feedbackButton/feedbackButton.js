455939e6eca59df8df283de8161598bbf85cbb61
tlmn <mail@tlmn.io>
2022-11-01 10:00:08 +0100

    fix button

--- src/components/feedbackButton/feedbackButton.js
+++ src/components/feedbackButton/feedbackButton.js
@@ -4,16 +4,20 @@ import React from "react";
 const FeedbackButton = () => {
   const { t } = useTranslation("common");
   return (
-    <a
-      href={`mailto:${t("feedbackMail")}?subject=${t(
-        "feedbackSubject"
-      )}&body=${encodeURIComponent(
-        t("feedbackBody", {
-          url: typeof window !== "undefined" ? window.location.href : "",
-        })
-      )}`}
-      dangerouslySetInnerHTML={{ __html: t("sendFeedback") }}
-    />
+    <>
+      {typeof window !== "undefined" && (
+        <a
+          href={`mailto:${t("feedbackMail")}?subject=${t(
+            "feedbackSubject"
+          )}&body=${encodeURIComponent(
+            t("feedbackBody", {
+              url: window.location.href,
+            })
+          )}`}
+          dangerouslySetInnerHTML={{ __html: t("sendFeedback") }}
+        />
+      )}
+    </>
   );
 };
 export default FeedbackButton;
