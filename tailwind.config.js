2acb9d687414980c1b4cd431cc8e07d280cd77c4
tlmn <mail@tlmn.io>
2022-11-04 17:18:10 +0100

    add fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,18 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./src/components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        noto: ["Noto Sans", ...defaultTheme.fontFamily.sans],
+        montserrat: ["Montserrat", ...defaultTheme.fontFamily.sans],
+        georgia: ["Georgia", ...defaultTheme.fontFamily.serif],
+      },
+    },
   },
   plugins: [],
 };
