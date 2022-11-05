8ce6d9d8e8d0b0455e2a7340e16a57bb1aca97ba
tlmn <mail@tlmn.io>
2022-11-05 20:02:42 +0100

    fix hydration error

--- src/components/feedbackButton/feedbackButton.js
+++ src/components/feedbackButton/feedbackButton.js
@@ -1,11 +1,19 @@
 import useTranslation from "next-translate/useTranslation";
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const FeedbackButton = () => {
+  // using the state to avoid hydration errors 
+  const [isVisible, setVisible] = useState(false);
+
+  useEffect(() => {
+    setVisible(true);
+  }, []);
+
   const { t } = useTranslation("common");
+
   return (
     <>
-      {typeof window !== "undefined" && (
+      {isVisible && (
         <a
           href={`mailto:${t("feedbackMail")}?subject=${t(
             "feedbackSubject"
