6c7992997e63174bca88f9c7f2ce996a1702ae14
tlmn <mail@tlmn.io>
2022-11-06 17:50:48 +0100

    safelist bg colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,7 @@ module.exports = {
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./src/components/**/*.{js,ts,jsx,tsx}",
   ],
+  safelist: ["bg-purple", "bg-yellow", "bg-green", "bg-red", "bg-blue"],
   theme: {
     extend: {
       fontFamily: {
