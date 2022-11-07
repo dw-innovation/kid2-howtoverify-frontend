77ecb175f89655b4b97ea758a9dd712bbf41ef02
tlmn <mail@tlmn.io>
2022-11-07 10:42:35 +0100

    add search

--- src/components/searchBox/searchBox.js
+++ src/components/searchBox/searchBox.js
@@ -1,9 +1,27 @@
+import { ROOTNODES } from "@/lib/const";
+import useTranslation from "next-translate/useTranslation";
 import React from "react";
+import LensIcon from "src/assets/svg/lens";
 
 const SearchBox = () => {
+  const { t } = useTranslation("common");
   return (
-    <div>
-      <input className="border-2 border-black" /> <button>Search</button>
+    <div className="flex flex-row gap-1 font-noto">
+      <input
+        className="rounded-l-md p-2 flex-1"
+        placeholder={t("searchBoxPlaceholder")}
+      />{" "}
+      <select name="category" className="bg-white h-full">
+        <option value="" disabled selected>
+          {t("searchCategory")}
+        </option>
+        {ROOTNODES.map((node, index) => (
+          <option value={node.id} key={index}>
+            {node.label}
+          </option>
+        ))}
+      </select>
+      <button className="rounded-r-md bg-blue hover:bg-darkBlue h-full px-2"><LensIcon/></button>
     </div>
   );
 };
