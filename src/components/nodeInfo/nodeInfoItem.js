6d2d65c2c860bdc7f6e38af6784ea5983ec0aa11
tlmn <mail@tlmn.io>
2022-11-08 14:09:18 +0100

    adapt node info

--- src/components/nodeInfo/nodeInfoItem.js
+++ src/components/nodeInfo/nodeInfoItem.js
@@ -1,17 +1,30 @@
+import useTranslation from "next-translate/useTranslation";
 import React from "react";
 
-const NodeInfoItem = ({ name, body }) => (
-  <>
-    {body !== "" && (
-      <div>
-        <span
-          dangerouslySetInnerHTML={{ __html: name }}
-          className="font-bold pr-2"
-        />
-        <span dangerouslySetInnerHTML={{ __html: body }} />
-      </div>
-    )}
-  </>
-);
+const NodeInfoItem = ({ name, body }) => {
+  const { t } = useTranslation("nodeInfo");
+
+  return (
+    <>
+      {body !== "" && (
+        <div className="flex flex-col pb-2">
+          {name !== "comment" && (
+            <span
+              dangerouslySetInnerHTML={{ __html: t(name) }}
+              className="font-bold"
+            />
+          )}
+          {name === "applicationUrl" ? (
+            <a href={body} target="_blank" rel="noopener noreferrer">
+              {body}
+            </a>
+          ) : (
+            <span dangerouslySetInnerHTML={{ __html: body }} />
+          )}
+        </div>
+      )}
+    </>
+  );
+};
 
 export default NodeInfoItem;
