dee52a0afcce1c69e3a7633a31239b780d9d48dd
tlmn <mail@tlmn.io>
2022-11-09 13:46:45 +0100

    add hide button

--- src/components/panel/panel.js
+++ src/components/panel/panel.js
@@ -1,16 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import About from "../about";
 import SearchBox from "../searchBox";
 import NodeInfo from "../nodeInfo";
+import clsx from "clsx";
 
 const Panel = () => {
+  const [isOpen, setIsOpen] = useState(true);
   return (
-    <div className="w-[30rem] bg-slate-200 p-2 flex gap-3 flex-col z-50 relative">
-      <SearchBox />
-      <div className="flex flex-col flex-1 justify-between">
-      <NodeInfo />
-      <About />
-      </div>
+    <div
+      className={clsx(
+        "bg-slate-200 p-2 flex gap-3 flex-col z-50 relative",
+        isOpen ? "w-[30rem]" : "w-[1.5rem]"
+      )}
+    >
+      <button
+        className="absolute top-0 bottom-0 left-0 -translate-x-1/2 bg-white h-max my-auto rounded-full shadow-sm font-bold"
+        style={{ height: "2rem", width: "2rem" }}
+        onClick={() => setIsOpen(!isOpen)}
+      >
+        {isOpen ? ">" : "<"}
+      </button>
+      {isOpen && (
+        <>
+          <SearchBox />
+          <div className="flex flex-col flex-1 justify-between">
+            <NodeInfo />
+            <About />
+          </div>
+        </>
+      )}
     </div>
   );
 };
