556f51f05a8527759a6723fe373b062b4227fba0
tlmn <mail@tlmn.io>
2022-11-23 18:20:00 +0100

    remove footer

--- src/components/panel/panel.js
+++ src/components/panel/panel.js
@@ -8,7 +8,6 @@ import useAppContext from "@/lib/hooks/useAppContext";
 import Color from "color";
 import SearchResults from "../searchResults";
 import ArrowRight from "src/assets/svg/arrow";
-import Footer from "../footer";
 
 const Panel = () => {
   const [isOpen, setIsOpen] = useState(true);
@@ -59,7 +58,6 @@ const Panel = () => {
           />
           <About className="bg-white" />
         </div>
-        <Footer />
       </div>
     </div>
   );
