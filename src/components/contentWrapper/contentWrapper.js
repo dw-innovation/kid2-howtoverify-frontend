a1b140f48246ecbd4613394363b3fe1ea951c29e
tlmn <mail@tlmn.io>
2022-12-14 18:05:23 +0100

    add blur animation

--- src/components/contentWrapper/contentWrapper.js
+++ src/components/contentWrapper/contentWrapper.js
@@ -1,16 +1,20 @@
 import React from "react";
 import useSessionStore from "@/lib/stores/useSessionStore";
+import { useSpring, animated } from "react-spring";
 
 const ContentWrapper = ({ children }) => {
   const modalIsOpen = useSessionStore((state) => state.modal.isOpen);
+  const blurStyle = useSpring({
+    filter: modalIsOpen ? `blur(4px)` : `blur(0px)`,
+  });
 
   return (
-    <div
+    <animated.div
       className="w-screen h-screen max-h-screen relative bg-lightGrey hidden md:flex flex-col"
-      style={{ filter: modalIsOpen ? "blur(4px)" : "" }}
+      style={blurStyle}
     >
       {children}
-    </div>
+    </animated.div>
   );
 };
 
