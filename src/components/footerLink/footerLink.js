6a4ac4e80bc6f23c4e180c7da79d898144d90ccf
tlmn <mail@tlmn.io>
2022-12-15 12:15:12 +0100

    increase spacing

--- src/components/footerLink/footerLink.js
+++ src/components/footerLink/footerLink.js
@@ -3,7 +3,7 @@ import React from "react";
 const FooterLink = ({ innerHTML, onClickAction }) => (
   <a
     dangerouslySetInnerHTML={{ __html: innerHTML }}
-    className="text-white hover:underline font-bold cursor-pointer px-2"
+    className="text-white hover:underline font-bold cursor-pointer px-4"
     onClick={onClickAction}
   />
 );
