f601a1f5cec632eed351229b0916b6c1bc6a8624
tlmn <mail@tlmn.io>
2022-12-19 22:55:08 +0100

    add text

--- src/components/privacySettingsManager/privacySettingsManager.js
+++ src/components/privacySettingsManager/privacySettingsManager.js
@@ -1,16 +1,19 @@
 import usePersistedStore from "@/lib/stores/usePersistedStore";
 import React from "react";
 import SettingsSwitch from "./settingsSwitch";
+import useTranslation from "next-translate/useTranslation";
+import ReactMarkdown from "react-markdown/lib/react-markdown";
 
 const SettingsManager = () => {
   const trackingEnabled = usePersistedStore((state) => state.trackingEnabled);
   const toggleTracking = usePersistedStore((state) => state.toggleTracking);
+  const { t } = useTranslation("common");
 
   return (
     <div>
-      <h1>Manage your privacy settings</h1>
+      <h1 dangerouslySetInnerHTML={{ __html: t("privacyManagerTitle") }} />
       <div className="pb-5 pl-5">
-        <p>Manage your consent to anonymous analytics on our site:</p>
+        <ReactMarkdown children={t("privacyManagerText")} />
         <SettingsSwitch
           currentState={trackingEnabled}
           label="analytics"
