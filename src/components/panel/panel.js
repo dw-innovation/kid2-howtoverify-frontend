74149e6962a36517ee72123fa2ea2db6833220c5
tlmn <mail@tlmn.io>
2023-01-10 16:59:22 +0100

    add sample animation

--- src/components/panel/panel.js
+++ src/components/panel/panel.js
@@ -7,6 +7,7 @@ import useSessionStore from "@/lib/stores/useSessionStore";
 import useWindowSize from "@/lib/hooks/useWindowSize";
 
 const Panel = () => {
+  const [notify, setNotify] = useState(false);
   const showPanel = useSessionStore((state) => state.showPanel);
   const togglePanel = useSessionStore((state) => state.togglePanel);
   const [panelHeight, setPanelHeight] = useState(0);
@@ -17,6 +18,13 @@ const Panel = () => {
   useEffect(() => {
     setPanelHeight(ref?.current?.clientHeight);
   }, [ref, width, height]);
+
+  useEffect(() => {
+    setNotify(true);
+    setTimeout(() => {
+      setNotify(false);
+    }, 2000);
+  }, [pathNodes]);
 
   return (
     <div
@@ -30,7 +38,9 @@ const Panel = () => {
       ref={ref}
     >
       <button
-        className="absolute top-0 bottom-0 left-0 z-20 my-auto flex h-max -translate-x-1/2 items-center justify-center rounded-full bg-blue-primary font-bold text-white shadow-sm transition-all duration-200 ease-in-out hover:bg-white hover:text-blue-primary"
+        className={clsx(
+          "absolute top-0 bottom-0 left-0 z-20 my-auto flex h-max -translate-x-1/2 items-center justify-center rounded-full bg-blue-primary font-bold text-white shadow-sm transition-all duration-200 ease-in-out hover:bg-white hover:text-blue-primary", notify && "animate-pulse"
+        )}
         style={{ height: "2rem", width: "2rem" }}
         onClick={() => togglePanel(!showPanel)}
         role="button"
