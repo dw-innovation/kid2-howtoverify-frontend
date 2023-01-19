b265a0bec2d669d556c35bd2e609e5ca5911d399
tlmn <mail@tlmn.io>
2023-01-19 11:55:21 +0100

    make padding permanent

--- src/components/graphCanvas/graphCanvas.js
+++ src/components/graphCanvas/graphCanvas.js
@@ -3,16 +3,14 @@ import GraphRenderer from "../graphRenderer";
 import Navigation from "../navigation";
 import MediaTypeSelector from "../mediaTypeSelector";
 import useSessionStore from "@/lib/stores/useSessionStore";
-import usePersistedStore from "@/lib/stores/usePersistedStore";
 import clsx from "clsx";
 
 const GraphCanvas = () => {
   const pathNodes = useSessionStore((state) => state.pathNodes);
-  const showFeatureTour = usePersistedStore((state) => state.showFeatureTour);
 
   return (
     <div className="flex flex-col flex-1">
-      <div className={clsx("flex flex-1 relative z-10 items-center justify-center", showFeatureTour && "pl-2")}>
+      <div className="flex flex-1 relative z-10 items-center justify-center pl-2">
         {pathNodes.length !== 0 ? <GraphRenderer /> : <MediaTypeSelector />}
       </div>
       <div className="w-full relative pl-2 lg:pl-8 p-2 bg-lightGrey">
