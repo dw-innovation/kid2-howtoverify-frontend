d4a55021be3cd5b9f045b1e04276fd890f49c08c
tlmn <mail@tlmn.io>
2023-01-20 09:23:11 +0100

    make tablet version prettier

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -4,6 +4,7 @@ import SearchBox from "../searchBox";
 import SearchResults from "../searchResults";
 import MediaTypeSelector from "../mediaTypeSelector";
 import useSessionStore from "@/lib/stores/useSessionStore";
+import clsx from "clsx";
 
 const Header = () => {
   const pathNodes = useSessionStore((state) => state.pathNodes);
@@ -12,20 +13,20 @@ const Header = () => {
 
   return (
     <div className="relative flex flex-col items-center border-b-[1px] bg-grey-header lg:flex-row">
-      <div className="flex h-full flex-col items-center justify-center py-4 pl-2 lg:items-start lg:pl-8 lg:text-left">
+      <div className="flex h-full flex-col items-center justify-center pt-4 lg:py-4 pl-0 lg:items-start lg:pl-8 text-center lg:text-left">
         <div id="featureTour-1">
           <h1
-            className="pb-3 font-georgia text-3xl font-bold text-grey-darker lg:text-5xl"
+            className="pb-1 lg:pb-3 font-georgia text-3xl font-bold text-grey-darker lg:text-5xl"
             dangerouslySetInnerHTML={{ __html: t("title") }}
           />
           <h2
-            className="pb-3 font-noto text-grey-dark 2xl:text-lg"
+            className="pb-0 lg:pb-3 font-noto text-grey-dark 2xl:text-lg"
             dangerouslySetInnerHTML={{ __html: t("subtitle") }}
           />
         </div>
       </div>
       <div className="flex flex-1 items-center justify-center py-2 px-2 lg:pr-8 h-full">
-        <div className="flex flex-1 items-center justify-center h-full">
+        <div className={clsx("flex flex-1 items-center justify-center h-full", pathNodes.length === 0 && "hidden")}>
           <MediaTypeSelector header />
         </div>
         <div className="relative w-72 xl:w-96">
