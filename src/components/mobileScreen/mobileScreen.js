795ce611af992026663add38d2bf834350453508
tlmn <mail@tlmn.io>
2023-02-03 16:34:31 +0100

    fix screen

--- src/components/mobileScreen/mobileScreen.js
+++ src/components/mobileScreen/mobileScreen.js
@@ -5,7 +5,7 @@ import Div100vh from "react-div-100vh";
 const MobileScreen = () => {
   const { t } = useTranslation("common");
   return (
-    <Div100vh className="md:hidden w-screen flex items-center justify-center flex-col p-3">
+    <Div100vh className="md:hidden w-screen flex items-center justify-center flex-col p-3 z-[9999] bg-white absolute">
       <h1 className="font-bold font-georgia text-blue-primary text-2xl mb-2">
         {t("mobileTitle")}
       </h1>
