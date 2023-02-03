36ade8b9f1fe469f81ae7104e8ac13ba4ba7fd57
tlmn <mail@tlmn.io>
2023-02-03 16:37:33 +0100

    fix footer link

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,37 +1,30 @@
-import usePersistedStore from '@/lib/stores/usePersistedStore'
-import useSessionStore from '@/lib/stores/useSessionStore'
-import useTranslation from 'next-translate/useTranslation'
-import React, { Fragment } from 'react'
-import FooterLink from '../footerLink'
+import usePersistedStore from "@/lib/stores/usePersistedStore";
+import useSessionStore from "@/lib/stores/useSessionStore";
+import useTranslation from "next-translate/useTranslation";
+import React, { Fragment } from "react";
+import { ReactMarkdown } from "react-markdown/lib/react-markdown";
+import FooterLink from "../footerLink";
 
 const Footer = () => {
-  const { t } = useTranslation('footer')
+  const { t } = useTranslation("footer");
 
-  const toggleModal = useSessionStore((state) => state.toggleModal)
-  const setModalContent = useSessionStore((state) => state.setModalContent)
-  const toggleFeatureTour = usePersistedStore(
-    (state) => state.toggleFeatureTour,
-  )
-  const toggleCookieBanner = usePersistedStore(
-    (state) => state.toggleCookieBanner,
-  )
+  const toggleModal = useSessionStore((state) => state.toggleModal);
+  const setModalContent = useSessionStore((state) => state.setModalContent);
 
   const handleClick = (e, target) => {
-    e.preventDefault()
-    toggleModal()
-    setModalContent(target)
-  }
+    e.preventDefault();
+    toggleModal();
+    setModalContent(target);
+  };
 
-  const LINKS = ['about', 'legal-privacy']
+  const LINKS = ["about", "legal-privacy"];
 
-  const currentYear = new Date().getFullYear()
+  const currentYear = new Date().getFullYear();
 
   return (
     <div className="font-noto text-sm md:text-md flex gap-5 justify-center py-2 bg-grey-darker">
-      <span
-        dangerouslySetInnerHTML={{
-          __html: t('copyright').replace('{{YEAR}}', currentYear),
-        }}
+      <ReactMarkdown
+        children={t("copyright").replace("{{YEAR}}", currentYear)}
         className="text-grey-light"
       />
       {LINKS.map((link, index) => (
@@ -43,7 +36,7 @@ const Footer = () => {
         </Fragment>
       ))}
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
