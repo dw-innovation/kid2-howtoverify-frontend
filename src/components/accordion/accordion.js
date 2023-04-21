9431bf725f8b9d94877e026be30ddf2de904c800
tlmn <mail@tlmn.io>
2023-04-21 14:25:47 +0200

    remove interactivity

--- src/components/accordion/accordion.js
+++ src/components/accordion/accordion.js
@@ -6,9 +6,14 @@ const Accordion = ({ title, children, style = {} }) => {
   const pathNodes = useSessionStore((state) => state.pathNodes);
 
   return (
-    <details className="w-full font-montserrat" style={style} open id="featureTour-5">
-      <summary
-        className="flex flex-row items-center px-6 py-4 text-xl font-bold cursor-pointer 2xl:text-2xl"
+    <div
+      className="w-full font-montserrat"
+      style={style}
+      open
+      id="featureTour-5"
+    >
+      <div
+        className="flex flex-row items-center px-6 py-4 text-xl font-bold 2xl:text-2xl"
         style={{
           color: getNodeColor(pathNodes[0], "primary"),
           borderLeft: "3px solid",
@@ -16,9 +21,9 @@ const Accordion = ({ title, children, style = {} }) => {
         }}
       >
         <h2 className="flex-1">{title}</h2>
-      </summary>
+      </div>
       <span className="block px-6 py-4 text-md 2xl:text-lg">{children}</span>
-    </details>
+    </div>
   );
 };
 
