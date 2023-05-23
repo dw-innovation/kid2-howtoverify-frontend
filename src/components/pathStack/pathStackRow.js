4cb4382506e016c231f4894010d14fa45a5c667e
tlmn <mail@tlmn.io>
2023-05-23 10:40:59 +0200

    darken

--- src/components/pathStack/pathStackRow.js
+++ src/components/pathStack/pathStackRow.js
@@ -3,7 +3,7 @@ import PathStackNode from "./pathStackNode";
 
 const PathStackRow = ({ nodes, level }) => {
   return (
-    <div className="flex justify-center w-full gap-2 px-2 overflow-x-scroll odd:bg-slate-100">
+    <div className="flex justify-center w-full gap-2 px-2 overflow-x-scroll odd:bg-slate-200">
       {nodes.map((node, index) => (
         <Fragment key={index}>
           <PathStackNode node={node} level={level} />
